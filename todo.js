4c2aedc3dc03388e2f533dda1e8a43f9d29b6750
Andrew <atandiawan@gmail.com>
2016-09-07 11:09:32 +0700

    pindah komputer

--- todo.js
+++ todo.js
@@ -1,1 +1,40 @@
+class Menu {
+	static addTask(){
 
+	}
+
+	static deleteTask(){
+
+	}
+
+	static displayTask(){
+
+	}
+}
+
+class Task {
+	constructor(input){
+		this._status = " "
+		this._namaTask = ||"default: Masukan nama task"
+	}
+
+	completeTask(){
+		this._status = "x"
+	}
+
+	uncompleteTask(){
+		this._status = " "
+	}
+}
+
+class KumpulanTask{
+	constructor(){
+		this._kumpulan = []
+	}
+
+	addTask(input){
+		this._kumpulan.push(new Task(input))
+	}
+}
+
+new todoku = new KumpulanTask()
